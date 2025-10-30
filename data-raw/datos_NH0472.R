b4a8d303821d5b41db168f7773843b411ac39538
Gaspi Alfonso <galfonso1@mail.austral.edu.ar>
2025-10-30 14:12:13 -0300

    Añade dataset meteorológico NH0472 en el paquete, ya funcional.

---
+++ data-raw/datos_NH0472.R
@@ -0,0 +1,9 @@
+# Cargamos los datos del paquete
+devtools::load_all
+
+# Descargamos los datos de la estación NH0472
+datos_NH0472 <- lectura_datos_estaciones("NH0472", "datosmeteo/NH0472.csv")
+
+# Guardamos el dataset dentro del paquete
+usethis::use_data(datos_NH0472, overwrite = TRUE)
+
