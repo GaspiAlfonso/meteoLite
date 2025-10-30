f15c1ac4496378738d4b4d09012db5452da6bd4d
Gaspi Alfonso <galfonso1@mail.austral.edu.ar>
2025-10-30 14:25:51 -0300

    Creamos dataset de estación NH0472 (se actualiza el nombre de función).

--- data-raw/datos_NH0472.R
+++ data-raw/datos_NH0472.R
@@ -2,7 +2,7 @@
 devtools::load_all
 
 # Descargamos los datos de la estación NH0472
-datos_NH0472 <- lectura_datos_estaciones("NH0472", "datosmeteo/NH0472.csv")
+datos_NH0472 <- descarga_data("NH0472", "datosmeteo/NH0472.csv")
 
 # Guardamos el dataset dentro del paquete
 usethis::use_data(datos_NH0472, overwrite = TRUE)
