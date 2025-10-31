60fa7825290efe8c51d354fac15f24649dadd777
Gaspi Alfonso <galfonso1@mail.austral.edu.ar>
2025-10-31 18:34:11 -0300

    Actualizo función visualtm para la web (con imágenes)

--- R/visualtm.R
+++ R/visualtm.R
@@ -11,6 +11,7 @@
 #' datos <- descarga_data("NH0472", "datosmeteo/NH0472.csv")
 #' visualtm(datos)
 #' }
+#' ![Gráfico de temperatura mensual](figures/grafico_visualtm.png)
 #' @export
 visualtm <- function(datos, colores = NULL, titulo = "Temperatura") {
 
